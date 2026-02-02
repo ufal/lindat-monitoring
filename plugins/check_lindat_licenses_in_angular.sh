178f09aa7c1052909fcfae1754f7c2a88b026381
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2026-02-02 11:03:36 +0100

    Adding a check specifically for lindat license texts hosted in angular repo

---
+++ plugins/check_lindat_licenses_in_angular.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o pipefail
+SCRIPTPATH=$(dirname $0)
+BASE=$1
+URLPATH=$2
+PATTERN=$3
+DEBUG=$4
+if ! which hxwls > /dev/null;then
+	exit 1
+fi
+#hxwls sometimes reports syntaxerrors, ignore it's error codes
+wget --quiet -O - "$BASE/$URLPATH" | (hxwls -b "$BASE" 2>&1 || true) | \
+egrep "$PATTERN" | sed -e 's#/static/#/static-files/#' | 
+while read -r line; do
+  if [ ! -z $DEBUG ]; then echo "DBG: $line"; fi
+  output=$($SCRIPTPATH/check_url_status -o -U $line) || {
+	echo "$output"
+	exit 1
+  }
+done
